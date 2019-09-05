30590eae4502f33de0f3b370ea84d5fa6c5ddc03
emmannweb <emmanuelnoc@gmail.com>
2019-09-05 00:07:50 -0300

    Updates

---
+++ precache-manifest.1ff9064be591d1ed2345952c07f8e92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb93765413734ffe5f574cc7fbfe454d",
+    "url": "/React-book-finder/index.html"
+  },
+  {
+    "revision": "a737a87d1674075ac609",
+    "url": "/React-book-finder/static/css/main.d4a52fca.chunk.css"
+  },
+  {
+    "revision": "eb82c7f1e8fd965c3589",
+    "url": "/React-book-finder/static/js/2.e6882a82.chunk.js"
+  },
+  {
+    "revision": "a737a87d1674075ac609",
+    "url": "/React-book-finder/static/js/main.5f408c82.chunk.js"
+  },
+  {
+    "revision": "581468be1c287aa1d2cc",
+    "url": "/React-book-finder/static/js/runtime~main.60973d69.js"
+  }
+]);
